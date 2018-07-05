3f6d70e33e2756af94b9ccc451e8ee6c6fe163ec
anntsit <anyatsit98@ukr.net>
2018-07-05 22:56:52 +0300

    rest

--- src/main/java/com/netcracker/boot/controller/HomeController.java
+++ src/main/java/com/netcracker/boot/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.netcracker.boot.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,12 +13,12 @@ public class HomeController {
 
     RestTemplate restTemplate = new RestTemplate();
     String fooResourceUrl
-            = "http://localhost:8080/spring-rest/foos";
+            = "http://localhost:8080/response";
 
-    @RequestMapping(value = "/response", method = RequestMethod.GET)
+    @RequestMapping(value = "/request", method = RequestMethod.GET)
     public String index() {
         ResponseEntity<String> response
-                = restTemplate.getForEntity(fooResourceUrl + "/1", String.class);
-        return "ok";
+                = restTemplate.getForEntity(fooResourceUrl , String.class);
+        return response.getBody().toString();
     }
 }
